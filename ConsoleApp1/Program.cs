7125616ea32e46077446fb9a7ac54f5fe524fcef
hackercracker5 <hackercracker@mail.bg>
2021-04-17 11:29:04 +0300

    Zadachle

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+
+namespace List
+{
+    public class CustomArrayList
+
+    {
+
+        private object[] arr;
+
+
+
+        private int count;
+
+        public int Count
+
+        {
+
+            get
+
+            {
+
+                return count;
+
+            }
+
+        }
+
+
+
+        private static readonly int INITIAL_CAPACITY = 4;
+
+        public CustomArrayList()
+
+        {
+
+            arr = new object[INITIAL_CAPACITY];
+
+            count = 0;
+
+        }
+        public void Add(object item)
+
+        {
+
+            Insert(count, item);
+
+        }
+
+        public void Insert(int index, object item)
+
+        {
+
+            if (index > count || index < 0)
+
+            {
+
+                throw new IndexOutOfRangeException(
+
+                      "Invalid index: " + index);
+
+            }
+
+            object[] extendedArr = arr;
+
+            if (count + 1 == arr.Length)
+
+            {
+
+                extendedArr = new object[arr.Length * 2];
+
+            }
+
+            Array.Copy(arr, extendedArr, index);
+
+            count++;
+
+            Array.Copy(arr, index, extendedArr, index + 1, count - index - 1);
+
+            extendedArr[index] = item;
+
+            arr = extendedArr;
+
+        }
+
+        public int IndexOf(object item)
+
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+
+            {
+
+                if (item == arr[i])
+
+                {
+
+                    return i;
+
+                }
+
+            }
+
+
+
+            return -1;
+
+        }
+
+
+
+        public void Clear()
+
+        {
+
+            arr = new object[INITIAL_CAPACITY];
+
+            count = 0;
+
+        }
+
+
+
+
+        public bool Contains(object item)
+
+        {
+
+            int index = IndexOf(item);
+
+            bool found = (index != -1);
+
+            return found;
+
+        }
+
+        public object this[int index]
+
+        {
+
+            get
+
+            {
+
+                if (index >= count || index < 0)
+
+                {
+
+                    throw new ArgumentOutOfRangeException(
+
+                          "Invalid index: " + index);
+
+                }
+
+                return arr[index];
+
+            }
+
+            set
+
+            {
+
+                if (index >= count || index < 0)
+
+                {
+
+                    throw new ArgumentOutOfRangeException(
+
+                          "Invalid index: " + index);
+
+                }
+
+                arr[index] = value;
+
+            }
+
+        }
+
+        public object Remove(int index)
+
+        {
+
+            if (index >= count || index < 0)
+
+            {
+
+                throw new ArgumentOutOfRangeException(
+
+                      "Invalid index: " + index);
+
+            }
+
+
+
+            object item = arr[index];
+
+            Array.Copy(arr, index + 1, arr, index, count - index + 1);
+
+            arr[count - 1] = null;
+
+            count--;
+
+
+
+            return item;
+
+        }
+
+        public int Remove(object item)
+
+        {
+
+            int index = IndexOf(item);
+
+            if (index == -1)
+
+            {
+
+                return index;
+
+            }
+
+
+
+            Array.Copy(arr, index + 1, arr, index, count - index + 1);
+
+            count--;
+
+
+
+            return index;
+
+        }
+        public static void Main()
+
+        {
+
+            CustomArrayList shoppingList = new CustomArrayList();
+
+            shoppingList.Add("Milk");
+
+            shoppingList.Add("Honey");
+
+            shoppingList.Add("Olives");
+
+            shoppingList.Add("Beer");
+
+            shoppingList.Remove("Olives");
+
+            Console.WriteLine("We need to buy:");
+
+            for (int i = 0; i < shoppingList.Count; i++)
+
+            {
+
+                Console.WriteLine(shoppingList[i]);
+
+            }
+
+            Console.WriteLine("Do we have to buy Bread? " +
+
+                  shoppingList.Contains("Bread"));
+
+        }
+
+
+    }
+}
---
+++ Queue/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+public class Queue
+{
+	private static int front, rear, capacity;
+	private static int[] queue;
+
+	public Queue(int c)
+	{
+		front = rear = 0;
+		capacity = c;
+		queue = new int[capacity];
+	}
+	public void queueEnqueue(int data)
+	{
+		if (capacity == rear)
+		{
+			Console.Write("\nQueue is full\n");
+			return;
+		}
+		else
+		{
+			queue[rear] = data;
+			rear++;
+		}
+		return;
+	}
+	public void queueDequeue()
+	{
+		if (front == rear)
+		{
+			Console.Write("\nQueue is empty\n");
+			return;
+		}
+		else
+		{
+			for (int i = 0; i < rear - 1; i++)
+			{
+				queue[i] = queue[i + 1];
+			} 
+			if (rear < capacity)
+				queue[rear] = 0;
+			rear--;
+		}
+		return;
+	}
+	public void queueDisplay()
+	{
+		int i;
+		if (front == rear)
+		{
+			Console.Write("\nQueue is Empty\n");
+			return;
+		}
+		for (i = front; i < rear; i++)
+		{
+			Console.Write(" {0} <-- ", queue[i]);
+		}
+		return;
+	}
+	public void queueFront()
+	{
+		if (front == rear)
+		{
+			Console.Write("\nQueue is Empty\n");
+			return;
+		}
+		Console.Write("\nFront Element is: {0}", queue[front]);
+		return;
+	}
+}
+
+public class StaticQueueinCsharp
+{
+	public static void Main(String[] args)
+	{
+		Queue q = new Queue(4);
+		q.queueDisplay();
+		q.queueEnqueue(20);
+		q.queueEnqueue(30);
+		q.queueEnqueue(40);
+		q.queueEnqueue(50);
+		q.queueDisplay(); 
+		q.queueEnqueue(60);
+		q.queueDisplay();
+		q.queueDequeue();
+		q.queueDequeue();
+		Console.Write("\n\nafter two node deletion\n\n");
+		q.queueDisplay();
+		q.queueFront();
+	}
+} 
